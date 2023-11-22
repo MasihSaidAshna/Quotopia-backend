f55e6a0d07b8fcc416b81fe1d82a98bb856f7d03
cihadozdemir <ciha0128@gmail.com>
2023-11-22 12:12:48 +0100

    dto entity osv...

--- src/main/java/com/example/quotopiabackend/repository/CategoryRepository.java
+++ src/main/java/com/example/quotopiabackend/repository/CategoryRepository.java
@@ -1,6 +1,8 @@
 package com.example.quotopiabackend.repository;
 
 
+import com.example.quotopiabackend.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
