53f92e39f2b90f135e7354d6cc5060edea591bfd
MasihSaidAshna <said0983@stud.kea.dk>
2023-11-22 11:25:51 +0100

    Quotopia start

---
+++ src/main/java/com/example/quotopiabackend/QuotopiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.quotopiabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotopiaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuotopiaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quotopiabackend/QuotopiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quotopiabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotopiaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
