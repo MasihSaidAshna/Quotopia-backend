801557b89900abd102a1ca475d5e22192644b3aa
cihadozdemir <ciha0128@gmail.com>
2023-11-26 23:04:01 +0100

    dto og dto converter

--- src/main/java/com/example/quotopiabackend/service/QuotesService.java
+++ src/main/java/com/example/quotopiabackend/service/QuotesService.java
@@ -1,7 +1,11 @@
 package com.example.quotopiabackend.service;
 
+import com.example.quotopiabackend.repository.QuotesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuotesService {
+
+
 }
