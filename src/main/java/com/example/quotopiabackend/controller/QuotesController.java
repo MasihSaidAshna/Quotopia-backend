a7ef11f53833ad43a1a4bebe119844afd5352c34
Jothom2912 <johannes1504@gmail.com>
2023-11-27 12:12:32 +0100

    Controller update

--- src/main/java/com/example/quotopiabackend/controller/QuotesController.java
+++ src/main/java/com/example/quotopiabackend/controller/QuotesController.java
@@ -4,8 +4,13 @@ import com.example.quotopiabackend.entity.Quotes;
 import com.example.quotopiabackend.service.QuotesService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@CrossOrigin
+@RestController
 public class QuotesController
 {
     private final QuotesService quotesService;
